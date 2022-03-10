02037aaf76576d90005744870187ee41fe7a14c3
GiselyKC <gikavalli@gmail.com>
2022-03-10 13:05:35 -0300

    alteraçao da page App.js, adicionado route login e settings

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
 import Login from './Pages/Login';
+import Settings from './Pages/Settings';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          <Login />
-        </p>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/settings" component={ Settings } />
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
